0890002c30b9b84abcc1a1ab67106d4d73d3730f
Rene Groothuis <renegrooth@gmail.com>
2023-06-28 18:53:17 +1200

    added font

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,15 @@ import data from '../public/data.json'
 function App() {
   console.log(data[0])
   return (
-    <main className="flex min-h-screen flex-row items-center justify-center bg-slate-200">
+    <main className="flex min-h-screen flex-row items-center justify-center bg-slate-200 font-hanken">
       <div className="flex bg-white rounded-2xl">
         <div className="bg-blue-500 overflow-hidden rounded-2xl p-10">
           <div className="max-w-[15rem] flex flex-col justify-center">
             <h1 className="flex justify-center">Your Result</h1>
             <div className="rounded-full  bg-blue-800 flex items-center justify-center">
-              <p className="">76/100</p>
+              <p className="">
+                <span>76</span>/100
+              </p>
             </div>
             <div className="flex justify-center flex-col items-center">
               <p>Great</p>
